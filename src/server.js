15f41b9ec4d6844f3c396d327632a94eee3ce713
Andres Mendoza <andres.david.mm@hotmail.com>
2023-01-04 10:52:52 -0500

    first commit

--- src/server.js
+++ src/server.js
@@ -1,13 +1,24 @@
 const app = require('./app');
-const sequelize = require('./utils/connection');
+const {exec} = require('child_process');
+
 
 const PORT = process.env.PORT || 8080;
 
+
 const main = async() => {
     try{
-        await sequelize.sync();
-        console.log("DB connected!");
         app.listen(PORT);
+        await new Promise((resolve, reject) => {
+            const migrate = exec(
+              'sequelize db:migrate',
+              {env: process.env},
+              err => (err ? reject(err): resolve())
+            );
+          
+            // Forward stdout+stderr to this process
+            migrate.stdout.pipe(process.stdout);
+            migrate.stderr.pipe(process.stderr);
+          });
         console.log(`Server running on port ${PORT}`);
     } catch(error){
         console.log(error);
